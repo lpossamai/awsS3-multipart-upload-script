4dafb9ecd82f9ac8ecdeed47e1dccf0a819ea8aa
Lucas Possamai <l.possamai@outlook.com>
2020-05-20 09:28:43 +1200

    Typo

--- multipart-file-upload-s3.sh
+++ multipart-file-upload-s3.sh
@@ -16,5 +16,5 @@ do
         ((part_number=part_number+1))
         md5=$(openssl md5 -binary ${f} | base64)
         aws s3api upload-part --bucket ${bucket} --key $key $profile --part-number $part_number --body ${f} --upload-id ${upload_id} --content-md5 ${md5} | tee -a logs/upload-part.$part_number-s3.log
-        cat logs/upload-part.$part_number-s3.log | awk -F'"' '{print $5}' | cut -f 1 -d '\' | awk NF | awk '{ print "{\"ETag\":\"" $1 "\",\"PartNumber\":\"" $part_number "\"}," }' >> logs/output-test.json
+        cat logs/upload-part.$part_number-s3.log | awk -F'"' '{print $5}' | cut -f 1 -d '\' | awk NF | awk '{ print "{\"ETag\":\"" $1 "\",\"PartNumber\":\"" '$part_number' "\"}," }' >> logs/output-test.json
 done
